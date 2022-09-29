9c54c3a16271a4ca5373479d0f1112d45f079527
CPDragone <88412926+CPDragone@users.noreply.github.com>
2022-09-29 13:40:06 -0400

    Test javascript file

---
+++ test.js
@@ -0,0 +1,14 @@
+const { Body } = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const secret = "yZfkZdIlrpwhmd9IHwMcX3MBMyjkdVGe"
+const apikey = "llq6HUNrfmBZa3VkQdAKNHS0eUZ1EFij"
+
+fetch('https://runsignup.com/Rest/races?api_key=llq6HUNrfmBZa3VkQdAKNHS0eUZ1EFij&api_secret=yZfkZdIlrpwhmd9IHwMcX3MBMyjkdVGe&format=json&events=T&race_headings=F&race_links=F&include_wa')
+  .then(response => {
+    return response.json();
+  })
+  .then(user => {
+    console.log(user);
+  })
+
