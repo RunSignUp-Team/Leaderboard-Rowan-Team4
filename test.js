0df4911ec6c28f2ca72789268dd74445cd138cf7
CPDragone <88412926+CPDragone@users.noreply.github.com>
2022-10-03 15:47:41 -0400

    Fixed issue w/ distinguishing event id's, displays ordered results info

--- test.js
+++ test.js
@@ -4,15 +4,46 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const secret = "yZfkZdIlrpwhmd9IHwMcX3MBMyjkdVGe"
 const apikey = "llq6HUNrfmBZa3VkQdAKNHS0eUZ1EFij"
 
-fetch('https://runsignup.com/Rest/races?api_key=llq6HUNrfmBZa3VkQdAKNHS0eUZ1EFij&api_secret=yZfkZdIlrpwhmd9IHwMcX3MBMyjkdVGe&format=json&events=T&race_headings=F&race_links=F&include_wa')
+// grabs race id and corresponding event id's from recent races with results
+fetch('https://runsignup.com/Rest/races?api_key=' + apikey + ' &api_secret=' + secret + '&format=json&events=T&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=T&distance_units=K')
   .then(response => {
     return response.json();
   })
   .then(user => {
 
-    for (let i = 0; i < 50; i++) {
+    // can display id's for 50 races, 10 for testing purposes
+    for (let i = 0; i < 10; i++) {
       raceObject = user.races[i];
-      console.log(raceObject.race.race_id);
-      console.log(raceObject.race.events[0].event_id);
+      console.log("\nRace ID: " + raceObject.race.race_id);
+
+      var events = "";
+
+      // some races have multiple events, group race id and their event id's together
+      // concatenating into string for console printing purposes, event id is an integer
+      for(let i = 0; raceObject.race.events[i] != null; i++) {
+          events = events + raceObject.race.events[i].event_id + " ";
+      }
+      
+      console.log("Corresponding Event ID's: " + events);
+
     }
+  })
+
+
+
+// grabs result info from a specified race id + event id combo, then displays place order, name & time of each finisher
+// race id and event id can be inserted into the link through concatenation like how apikey and secret are
+fetch('https://runsignup.com/Rest/race/123467/results/get-results?api_key=' + apikey + '&api_secret=' + secret + '&format=json&event_id=567423&include_total_finishers=T&include_split_time_ms=F&supports_nb=F&page=1&results_per_page=50')
+  .then(response => {
+    return response.json();
+   })
+  .then(user => {
+  
+    console.log("Place\t" + "Name\t" + "\t\tTime\t")
+
+    for (let i = 0; i < user.individual_results_sets[0].num_finishers; i++){
+      playerObject = (user.individual_results_sets[0].results[i]);
+      console.log(playerObject.place + "\t" + playerObject.first_name + " " + playerObject.last_name + "\t\t" +playerObject.clock_time);
+    }
+
   })
