5d8841925a7be89a6d4d8ada85adbdf37f4fdbd7
ethane103 <ethane103@gmail.com>
2022-10-12 12:13:58 -0400

    Got db populated using API

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 const secret = "yZfkZdIlrpwhmd9IHwMcX3MBMyjkdVGe"
 const apikey = "llq6HUNrfmBZa3VkQdAKNHS0eUZ1EFij"
-let array1 = [];
+var array1 = [];
 
 // grabs result info from a specified race id + event id combo, then displays place order, name & time of each finisher
 // race id and event id can be inserted into the link through concatenation like how apikey and secret are
@@ -18,29 +18,28 @@ fetch('https://runsignup.com/Rest/race/21/results/get-results?api_key=' + apikey
 
     for (let i = 0; i < user.individual_results_sets[0].num_finishers; i++){
       playerObject = (user.individual_results_sets[0].results[i]);
-      array1.push.apply(playerObject.place);
+      array1[i] = [playerObject.place, playerObject.first_name, playerObject.last_name, playerObject.chip_time];
     }
     
-    console.log(array1);
+    console.log(array1[1]);
   const sqlite3 = require('sqlite3').verbose();
 
   let db = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE, (err) => {
       if (err){
           return console.error(err.message);
       }
-      console.log('Connected to the SQlite database');
   });
   
   // create the statement for the insertion of just ONE record
   let artistQuery = 
-    "INSERT INTO Test (place, first_name, last_name, time) " +
-    "VALUES (?, ?, ? ,?)"; 
+    "INSERT INTO Test (place, first_name, last_name, result_time ) " +
+    "VALUES (?, ?, ?, ?)"; 
   
   let statement = db.prepare(artistQuery);
   
   
   for (var i = 0; i < array1.length; i++) {
-      statement.run(array[i], function (err) { 
+      statement.run(array1[i], function (err) { 
           if (err) throw err;
       });
   }
