eecf6f9b701087bc8099132ab3030641a6df1d9c
CPDragone <88412926+CPDragone@users.noreply.github.com>
2022-10-17 13:08:26 -0400

    Organizes code into functions, fixes problem of database being busy with async & await

--- display.js
+++ display.js
@@ -12,10 +12,12 @@ let db = new sqlite3.Database('db.db', sqlite3.OPEN_READ, (err) => {
 let query = 
   `select place, first_name, last_name, result_time from Racers_Result`; // Cannot use ? for some reason to input which fields we want returned
 
+
 db.each(query, (err, row) => {
     if (err) {
         throw err;
     }
+
     console.log(`${row.place} ${row.first_name} ${row.last_name} ${row.result_time}` );
 });
 
--- test.js
+++ test.js
@@ -3,78 +3,104 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 const secret = "yZfkZdIlrpwhmd9IHwMcX3MBMyjkdVGe"
 const apikey = "llq6HUNrfmBZa3VkQdAKNHS0eUZ1EFij"
-var array1 = [];
-var array2 = [];
+var raceData = [];
+var eventData = [];
+var resultData = [];
 
-fetch('https://runsignup.com/Rest/races?api_key=' + apikey + ' &api_secret=' + secret + '&format=json&events=T&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=T&distance_units=K')
+function initDB() {
+
+  const sqlite3 = require('sqlite3').verbose();
+
+  let database = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE, (err) => {
+      if (err){
+          return console.error(err.message);
+      }
+  }); 
+
+  return database;
+
+}
+
+function populate(insertionQuery, array, db) {
+
+  let statement = db.prepare(insertionQuery)
+
+  for (var i = 0; i < array.length; i++) 
+  {
+      statement.run(array[i], function (err) { 
+          if (err) throw err;
+      });
+  }
+
+}
+
+
+function getRacesAndEvents(user) {
+
+  // can display id's for 50 races, 10 for testing purposes
+  for (let i = 0; i < 10; i++) {
+    raceObject = user.races[i];
+    raceData[i] = [raceObject.race.race_id, raceObject.race.name]
+    // console.log(i + "  \nRace ID: " + raceObject.race.race_id, raceObject.race.name);
+    
+    // some races have multiple events, group race id and their event id's together
+    // concatenating into string for console printing purposes, event id is an integer
+    for(let j = 0; raceObject.race.events[j] != null; j++) {
+        eventData.push([raceObject.race.race_id, raceObject.race.events[j].event_id , raceObject.race.events[j].name]);
+    }
+        
+  
+  }
+
+}
+
+function getRaceResults(user) {
+
+  for (let i = 0; i < user.individual_results_sets[0].num_finishers; i++){
+    playerObject = (user.individual_results_sets[0].results[i]);
+    resultData[i] = [playerObject.place, playerObject.first_name, playerObject.last_name, playerObject.chip_time];
+  }
+  
+}
+
+
+
+
+async function processRacesAndEvents() {
+  fetch('https://runsignup.com/Rest/races?api_key=' + apikey + ' &api_secret=' + secret + '&format=json&events=T&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=T&distance_units=K')
   .then(response => {
     return response.json();
   })
   .then(user => {
 
-    // can display id's for 50 races, 10 for testing purposes
-    for (let i = 0; i < 10; i++) {
-      raceObject = user.races[i];
-      array1[i] = [raceObject.race.race_id, raceObject.race.name]
-     // console.log(i + "  \nRace ID: " + raceObject.race.race_id, raceObject.race.name);
-
-      var events = "";
-
-      // some races have multiple events, group race id and their event id's together
-      // concatenating into string for console printing purposes, event id is an integer
-      for(let j = 0; raceObject.race.events[j] != null; j++) {
-          array2.push([raceObject.race.race_id, raceObject.race.events[j].event_id , raceObject.race.events[j].name]);
-      }
-      
-
-    }
-
+    getRacesAndEvents(user);
     const sqlite3 = require('sqlite3').verbose();
 
-  let db = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE, (err) => {
-      if (err){
-          return console.error(err.message);
-      }
-  });
+    let db = initDB();
   
-  // create the statement for the insertion of just ONE record
-  let insertionQuery = 
-    "INSERT INTO Event (race_id, event_id, event_name) " +
-    "VALUES (?, ?, ?)"; 
-  
-  let statement = db.prepare(insertionQuery);
-  
-  
-  for (var i = 0; i < array2.length; i++) 
-  {
-      statement.run(array2[i], function (err) { 
-          if (err) throw err;
-      });
-  }
+    // create the statement for the insertion of just ONE record
+    let insertionQuery = 
+      "INSERT INTO Event (race_id, event_id, event_name) " +
+      "VALUES (?, ?, ?)"; 
 
-  let insertionQuery2 =    
+    
+   
+    populate(insertionQuery, eventData, db);
+
+    let insertionQuery2 =    
     "INSERT INTO Races (race_id, race_name) " +
     "VALUES (?, ?)"; 
 
+    populate(insertionQuery2, raceData, db);
 
-  let statement2 = db.prepare(insertionQuery2);
-
-  for (var i = 0; i < array1.length; i++) 
-  {
-      statement2.run(array1[i], function (err) { 
-          if (err) throw err;
-      });
-  }
 });
+return;
+}
 
-  
+async function processResults() {
+  await processRacesAndEvents();
 
-
-
-// grabs result info from a specified race id + event id combo, then displays place order, name & time of each finisher
-// race id and event id can be inserted into the link through concatenation like how apikey and secret are
-/*
-fetch('https://runsignup.com/Rest/race/21/results/get-results?api_key=' + apikey +'&api_secret=' + secret + '&format=json&event_id=537625&include_total_finishers=T&include_split_time_ms=F&supports_nb=F&page=1&results_per_page=500')
+  fetch('https://runsignup.com/Rest/race/21/results/get-results?api_key=' + apikey +'&api_secret=' + secret + '&format=json&event_id=537625&include_total_finishers=T&include_split_time_ms=F&supports_nb=F&page=1&results_per_page=1000')
   .then(response => {
     return response.json();
    })
@@ -83,35 +109,24 @@ fetch('https://runsignup.com/Rest/race/21/results/get-results?api_key=' + apikey
     console.log("Place\t" + "Name\t" + "\t\tTime\t")
     
 
-    for (let i = 0; i < user.individual_results_sets[0].num_finishers; i++){
-      playerObject = (user.individual_results_sets[0].results[i]);
-      array1[i] = [playerObject.place, playerObject.first_name, playerObject.last_name, playerObject.chip_time];
-    }
+    getRaceResults(user);
     
-    console.log(array1[1]);
-  const sqlite3 = require('sqlite3').verbose();
+    console.log(resultData[1]);
+    const sqlite3 = require('sqlite3').verbose();
 
-  let db = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE, (err) => {
-      if (err){
-          return console.error(err.message);
-      }
-  });
+    let db = initDB();
   
-  // create the statement for the insertion of just ONE record
-  let insertionQuery = 
-    "INSERT INTO Racers_Result (place, first_name, last_name, result_time ) " +
-    "VALUES (?, ?, ?, ?)"; 
+    // create the statement for the insertion of just ONE record
+    let insertionQuery = 
+     "INSERT INTO Racers_Result (place, first_name, last_name, result_time ) " +
+     "VALUES (?, ?, ?, ?)"; 
   
-  let statement = db.prepare(insertionQuery);
+    let statement = db.prepare(insertionQuery);
   
   
-  for (var i = 0; i < array1.length; i++) 
-  {
-      statement.run(array1[i], function (err) { 
-          if (err) throw err;
-      });
-  }
+    populate(insertionQuery, resultData, db);
+
 });
+}
 
-  
-*/
+processResults();
