9737e03f0ba2abc25167a25b811e349cd3290c36
JOhn H <huynhj94@students.rowan.edu>
2022-11-01 13:24:53 -0400

    page1 changes

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+import logo from './runsignup_logo.png';
 import './App.css';
+import { RacesDropdown } from './Components/races-dropdown.js'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <br></br><br></br><br></br>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <br></br><br></br><br></br>
+        {/*<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -18,9 +21,40 @@ function App() {
         >
           Learn React
         </a>
+  */}
+
+      <div id="submit form">
+      <form method="POST" action="page2.html">
+  <center>
+    <label htmlFor="race-names">Choose a Race name: </label>
+    <RacesDropdown/>
+    <label htmlFor="event-names">Choose an Event name: </label>
+    <select name="event-names" id="event-names">
+      <option value="event1">1k</option>
+      <option value="event2">2k</option>
+      <option value="event3">3k</option>
+      <option value="event4">4k</option>
+      <option value="event5">5k</option>
+      <option value="event6">6k</option>
+      <option value="event7">7k</option>
+      <option value="event8">8k</option>
+      <option value="event8">9k</option>
+    </select>
+    <br />
+    <br />
+    <input className="submit" type="submit" defaultValue="Submit" />
+  </center>
+</form>
+
+      </div>
+
       </header>
     </div>
+
+
+
   );
+  
 }
 
 
---
+++ leaderboard/src/Components/races-dropdown.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class RacesDropdown extends Component {
+    render() {
+        return(    <select name="race-names" id="race-names">
+        <option value="race1">Race 1</option>
+        <option value="race2">Race 2</option>
+        <option value="race3">Race 3</option>
+        <option value="race4">Race 4</option>
+        <option value="race5">Race 5</option>
+         </select>)   
+    }
+   
+
+}
