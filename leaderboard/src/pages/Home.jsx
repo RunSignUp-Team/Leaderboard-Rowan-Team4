4215cc017e261b19f24ab0d6089170add042242d
JOhn H <huynhj94@students.rowan.edu>
2022-11-08 17:31:15 -0500

    linked pages

--- leaderboard/src/pages/Home.jsx
+++ leaderboard/src/pages/Home.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import logo from '../runsignup_logo.png';
+import  Dropdown  from '../Components/races-dropdown.js'
+import { Populate } from '../Components/populateButton.js';
+import { Reset } from '../Components/resetButton.js'
+import SubmitButton from '../Components/page1Submit.js'
+
 
 function Home() {
     return (
-        <h1>this is the homepage</h1>
+       <>
+       <center>
+        <h1>Welcome to the RSU Home Page</h1>
+        <br></br>
+        <img src={logo} alt="Logo" />
+        <br></br>
+        <Dropdown />
+              <br />
+              <br />
+        <SubmitButton />
+
+        <br />
+            <Populate />
+            <br />
+            <Reset />
+
+        </center>
+
+        
+        </>
+        
     );
 }
 
