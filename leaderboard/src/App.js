3b1108aed5f9d14706fd0cf2817c690cab040ed4
CPDragone <chrispauldragone@gmail.com>
2022-11-08 19:40:41 -0500

    switching pages with buttons, updated nav bar

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -1,15 +1,8 @@
-import { Populate } from './Components/populateButton.js';
-import logo from './runsignup_logo.png';
-import './App.css';
-import  Dropdown  from './Components/races-dropdown.js'
-import { Reset } from './Components/resetButton.js'
-import SubmitButton from './Components/page1Submit.js'
-/*import { Testing } from './test.js'*/
+
 
 /*import { Redirect } from "react-router";*/
 import { Switch } from "react-router";
 
-import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -21,56 +14,21 @@ import NoPage from "./pages/NoPage";
 function App() {
 
   return (
-    <><div className="App">
-      <header className="App-header">
-        <br></br><br></br><br></br>
-        <img src={logo} className="App-logo" alt="logo" />
-        <br></br><br></br><br></br>
-        {/*<p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-*/}
-
-        <div id="submit form">
-          <form method="POST" action="index.html">
-            <center>
-              <Dropdown />
-              <br />
-              <br />
-
-            </center>
-            <SubmitButton />
-            
-            <br />
-            <Populate />
-            <br />
-            <Reset />
-
-          </form>
-
-        </div>
-
-
-      </header>
-    </div>
-      </>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
   );
   
   
 }
-
-
-
 
 
 export default App;
--- leaderboard/src/pages/Home.jsx
+++ leaderboard/src/pages/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import logo from '../runsignup_logo.png';
+import '../App.css';
+import  Dropdown  from '../Components/races-dropdown.js'
+import { Reset } from '../Components/resetButton.js'
+import SubmitButton from '../Components/page1Submit.js'
+import { Populate } from '../Components/populateButton.js';
 
 function Home() {
     return (
-        <h1>this is the homepage</h1>
-    );
+        <><div className="App">
+        <header className="App-header">
+          <br></br><br></br><br></br>
+          <img src={logo} className="App-logo" alt="logo" />
+          <br></br><br></br><br></br>
+  
+          <div id="submit form">
+            <form method="POST" action="index.html">
+              <center>
+                <Dropdown />
+                <br />
+                <br />
+  
+              </center>
+              <SubmitButton />
+              
+              <Populate />
+              <br />
+              <Reset />
+  
+            </form>
+  
+          </div>
+  
+  
+        </header>
+      </div>
+        </>    );
 }
 
 export default Home;
