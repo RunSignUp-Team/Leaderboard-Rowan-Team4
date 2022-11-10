96bdcb0cd15a26187f2f34d1e00d8f6888a15453
JOhn H <huynhj94@students.rowan.edu>
2022-11-10 15:01:39 -0500

    updated format of page2

--- leaderboard/src/pages/page2.jsx
+++ leaderboard/src/pages/page2.jsx
@@ -3,13 +3,11 @@ import '../App.css';
 import Checkboxes from '../Components/Page2/Checkboxes'
 import Body from '../Components/Page2/Body'
 import Header from '../Components/Page2/Header'
-import Navigation from '../Components/Page2/Navigation'
 
 function Page2 () {
  
     return (
       <><div className="App">
-        <Navigation />
         <br>
         </br>
         <Header />
